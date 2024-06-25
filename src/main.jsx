f5375550a869fb4cc6cd39ab8eeb0fcc98a081ad
Chineze Okadigbo <72756219+neze14@users.noreply.github.com>
2024-06-25 09:04:22 +0100

    added signature

--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,20 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
+
+console.log(`
+         _               _       
+        /\\ \\           /\\ \\     
+       /  \\ \\         /  \\ \\    
+      / /\\ \\ \\       / /\\ \\ \\   
+     / / /\\ \\ \\     / / /\\ \\ \\  
+    / / /  \\ \\_\\   / / /  \\ \\_\\ 
+   / / /    \\/_/  / / /   / / / 
+  / / /          / / /   / / /  
+ / / /________  / / /___/ / /   
+/ / /_________\\/ / /____\\/ /    
+\\/____________/\\/_________/     
+                                
+
+  By Chineze Okadigbo: https://chineze-okadigbo.netlify.app
+`)
